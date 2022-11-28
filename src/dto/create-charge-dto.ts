af2b9663bdd881d1df137a64273e4639d8f5e0e7
Bruno Aguiar <bruno.aguiar@wisereducacao.com>
2022-11-28 09:54:41 -0300

    feat: create DTO

--- src/dto/create-charge-dto.ts
+++ src/dto/create-charge-dto.ts
@@ -13,7 +13,5 @@ export interface CreateChargeDTO {
   originType: string;
 }
 
-// transferindo um objeto de uma camada para outra
-
 // ex: Camada controller onde bate requisição do usuario, e se comunica com camada de dominio.
 // sabendo os dados que vão ser trasmitido de uma camada para outra.
