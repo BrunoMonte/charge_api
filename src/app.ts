4e1c63aeac828dd20b87eccfbaeff7bf969230a0
Bruno Aguiar <bruno.aguiar@wisereducacao.com>
2022-12-07 17:34:18 -0300

    feat: add validation AppDataSource typeOrm

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import { AppDataSource } from "./data-source";
 import { router } from "./routes";
 
-const app = express();
+AppDataSource.initialize().then(() => {
+  const app = express();
 
-app.use(express.json());
-app.use(router);
+  app.use(express.json());
+  app.use(router);
 
-export { app };
+  app.listen(process.env.PORT_APPLICATION);
+});
+
+//export { app };
